7fb005c96934b2eb3163daba63e2d578d03afa85
ilyasss001 <kuanyshbeklord17@gmail.com>
2026-01-29 20:27:24 +0500

    In progress

--- Pract01/strings.py
+++ Pract01/strings.py
@@ -1,5 +1,16 @@
-#1
+#1 string
 print("hello world")
-#2
+#2 slice to the end
 s = "awesome"
 print(s[3:])
+#3 modify strings
+a = "Hello, World!"
+print(a.lower())
+#4 replace string
+a = "Hello, World!"
+print(a.replace("H", "J"))
+#5 String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
