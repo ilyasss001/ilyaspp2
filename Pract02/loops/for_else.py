39b66febc941011ebd8ad17bcf67a2537a8bfc4d
ilyasss001 <kuanyshbeklord17@gmail.com>
2026-02-02 17:43:53 +0500

    In progress

---
+++ Pract02/loops/for_else.py
@@ -0,0 +1,13 @@
+#1
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+#2
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+#If the loop breaks, the else block is not executed.
---
+++ Pract02/loops/for_nested.py
@@ -0,0 +1,7 @@
+#1
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
