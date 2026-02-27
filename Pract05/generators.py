040032aee8121bbe935b5ff7776aac9808173db6
ilyasss001 <kuanyshbeklord17@gmail.com>
2026-02-27 17:32:14 +0500

    In progress

---
+++ Pract05/generators.py
@@ -0,0 +1,106 @@
+#1 iterators
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+#2
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+#3
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+#4
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+#5
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
+#1 generators
+def my_generator():
+  yield 1
+  yield 2
+  yield 3
+
+for value in my_generator():
+  print(value)
+#2
+def count_up_to(n):
+  count = 1
+  while count <= n:
+    yield count
+    count += 1
+
+for num in count_up_to(5):
+  print(num)
+#3
+def large_sequence(n):
+  for i in range(n):
+    yield i
+
+# This doesn't create a million numbers in memory
+gen = large_sequence(1000000)
+print(next(gen))
+print(next(gen))
+print(next(gen))
+#4
+def simple_gen():
+  yield "Emil"
+  yield "Tobias"
+  yield "Linus"
+
+gen = simple_gen()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+#5
+# List comprehension - creates a list
+list_comp = [x * x for x in range(5)]
+print(list_comp)
+
+# Generator expression - creates a generator
+gen_exp = (x * x for x in range(5))
+print(gen_exp)
+print(list(gen_exp))
